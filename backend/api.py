8f265f5fde5c419ba02dd2b9edfc967301c43ad0
MrWizz305 <147549743+MrWizz305@users.noreply.github.com>
2025-10-05 15:09:51 -0400

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all for now
-    allow_credentials=True
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
